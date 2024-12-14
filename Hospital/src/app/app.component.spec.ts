cd278470036b354a3f81da86b048de200e854eb6
Gailwad Sonali <gaikwadsonali2001@gmail.com>
2024-12-14 15:53:40 +0530

    Update in code

--- Hospital/src/app/app.component.spec.ts
+++ Hospital/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Hospital' title`, () => {
+  it(`should have the 'hospital' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Hospital');
+    expect(app.title).toEqual('hospital');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Hospital');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, hospital');
   });
 });
